bbb1d4fe8e435bd7c2d284bdb969a42265ea6993
Dillan <dillanp02@gmail.com>
2023-12-06 22:15:03 +0000

    Further socials page changes for hover effects

--- my-app/src/pages/SocialMedia.tsx
+++ my-app/src/pages/SocialMedia.tsx
@@ -12,13 +12,26 @@ import { TitleText } from "../components/Text/TitleText";
 import { BackgroundImage } from "../components/Images/BackgroundImage";
 
 import spotify_img from "../images/spotify-premium.gif";
-import spotify_frame from "../images/spotify-first-frame.png";
 import twitter_img from "../images/x-twitter.gif";
 import youtube_img from "../images/youtube.gif";
-import youtube_frame from "../images/youtube-first-frame.png";
+import white_img from "../images/white.png";
+import snapchat_img from "../images/snapchat-logo.gif";
+import discord_img from "../images/discord-logo.gif";
+import black_img from "../images/black.png";
+import instagram_img from "../images/instagram.gif";
+import bereal_img from "../images/BeReal.gif";
 
 export default function SocialMedia() {
-	type Tile = "spotify" | "twitter" | "discord" | "youtube" | "";
+	type Tile =
+		| "spotify"
+		| "twitter"
+		| "discord"
+		| "youtube"
+		| "snapchat"
+		| "insta"
+		| "bereal"
+		| "";
+
 	const [currentPhoto, setCurrentPhoto] = useState<Tile>("");
 
 	return (
@@ -33,37 +46,53 @@ export default function SocialMedia() {
 			>
 				<TitleText>Spotify</TitleText>
 				<BackgroundImage
-					src={currentPhoto === "spotify" ? spotify_img : spotify_frame}
+					src={currentPhoto === "spotify" ? spotify_img : black_img}
 					show={currentPhoto === "spotify"}
 				/>
 			</ExtraWideTallMasonaryTile>
 
-			<MasonaryTile>
+			<MasonaryTile onMouseEnter={() => setCurrentPhoto("snapchat")}>
 				<TitleText>Snapchat</TitleText>
+				<BackgroundImage
+					src={snapchat_img}
+					show={currentPhoto === "snapchat"}
+				/>
 			</MasonaryTile>
 
-			<ExtraTallMasonaryTile>
+			<ExtraTallMasonaryTile onMouseEnter={() => setCurrentPhoto("discord")}>
 				<TitleText>Discord</TitleText>
+				<BackgroundImage
+					src={currentPhoto === "discord" ? discord_img : black_img}
+					show={currentPhoto === "discord"}
+				/>
 			</ExtraTallMasonaryTile>
 
 			<MasonaryTile onMouseEnter={() => setCurrentPhoto("youtube")}>
 				<TitleText>YouTube</TitleText>
 				<BackgroundImage
-					src={currentPhoto === "youtube" ? youtube_img : youtube_frame}
+					src={currentPhoto === "youtube" ? youtube_img : white_img}
 					show={currentPhoto === "youtube"}
 				/>
 			</MasonaryTile>
 
-			<WideMasonaryTile>
-				<TitleText>Instagram</TitleText>
+			<WideMasonaryTile onMouseEnter={() => setCurrentPhoto("")}>
+				<TitleText>Letterboxd</TitleText>
 			</WideMasonaryTile>
 
-			<TallMasonaryTile>
+			<TallMasonaryTile onMouseEnter={() => setCurrentPhoto("bereal")}>
 				<TitleText>Bereal</TitleText>
+				<BackgroundImage
+					src={currentPhoto === "bereal" ? bereal_img : black_img}
+					show={currentPhoto === "bereal"}
+				/>
 			</TallMasonaryTile>
 
-			<WideTallMasonaryTile>
-				<TitleText>Letterboxd</TitleText>
+			<WideTallMasonaryTile onMouseEnter={() => setCurrentPhoto("insta")}>
+				<TitleText>Instagram</TitleText>
+				<BackgroundImage
+					src={currentPhoto === "insta" ? instagram_img : white_img}
+					show={currentPhoto === "insta"}
+				/>
 			</WideTallMasonaryTile>
 
 			<MasonaryTile>
