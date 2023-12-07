7196cf571bcf8738439ab433cb3c1a8c894043a4
Dillan <dillanp02@gmail.com>
2023-12-07 20:59:20 +0000

    Finishing links to all social medias

---
+++ my-app/src/components/Navigation/LinkWrapper.ts
@@ -0,0 +1,6 @@
+import styled from "styled-components";
+
+export const LinkWrapper = styled.a`
+	text-decoration: none;
+	color: inherit;
+`;
--- my-app/src/pages/SocialMedia.tsx
+++ my-app/src/pages/SocialMedia.tsx
@@ -17,9 +17,16 @@ import youtube_img from "../images/youtube.gif";
 import white_img from "../images/white.png";
 import snapchat_img from "../images/snapchat-logo.gif";
 import discord_img from "../images/discord-logo.gif";
+import discord_blue from "../images/discord-frame.png";
 import black_img from "../images/black.png";
 import instagram_img from "../images/instagram.gif";
 import bereal_img from "../images/BeReal.gif";
+import letterboxd_img from "../images/letterboxd.png";
+import linkedin_img from "../images/linkedin.gif";
+import reddit_img from "../images/reddit.gif";
+import twitch_img from "../images/twitch.gif";
+import twitch_frame from "../images/twitch-frame.png";
+import { LinkWrapper } from "../components/Navigation/LinkWrapper";
 
 export default function SocialMedia() {
 	type Tile =
@@ -30,6 +37,10 @@ export default function SocialMedia() {
 		| "snapchat"
 		| "insta"
 		| "bereal"
+		| "letterboxd"
+		| "linkedin"
+		| "reddit"
+		| "twitch"
 		| "";
 
 	const [currentPhoto, setCurrentPhoto] = useState<Tile>("");
@@ -37,75 +48,116 @@ export default function SocialMedia() {
 	return (
 		<MasonaryContainer>
 			<MasonaryTile onMouseEnter={() => setCurrentPhoto("twitter")}>
-				<TitleText>Twitter</TitleText>
-				<BackgroundImage src={twitter_img} show={currentPhoto === "twitter"} />
+				<LinkWrapper href="https://twitter.com/Dillanp02">
+					<TitleText>Twitter</TitleText>
+					<BackgroundImage
+						src={twitter_img}
+						show={currentPhoto === "twitter"}
+					/>
+				</LinkWrapper>
 			</MasonaryTile>
 
 			<ExtraWideTallMasonaryTile
 				onMouseEnter={() => setCurrentPhoto("spotify")}
 			>
-				<TitleText>Spotify</TitleText>
-				<BackgroundImage
-					src={currentPhoto === "spotify" ? spotify_img : black_img}
-					show={currentPhoto === "spotify"}
-				/>
+				<LinkWrapper href="https://open.spotify.com/user/vxgobr5jc5716irxzok7jrsbg">
+					<TitleText>Spotify</TitleText>
+					<BackgroundImage
+						src={currentPhoto === "spotify" ? spotify_img : black_img}
+						show={currentPhoto === "spotify"}
+					/>
+				</LinkWrapper>
 			</ExtraWideTallMasonaryTile>
 
 			<MasonaryTile onMouseEnter={() => setCurrentPhoto("snapchat")}>
-				<TitleText>Snapchat</TitleText>
-				<BackgroundImage
-					src={snapchat_img}
-					show={currentPhoto === "snapchat"}
-				/>
+				<LinkWrapper href="https://www.snapchat.com/add/dillanp2002?share_id=0PXgnfbYsds&locale=en-GB">
+					<TitleText>Snapchat</TitleText>
+					<BackgroundImage
+						src={snapchat_img}
+						show={currentPhoto === "snapchat"}
+					/>
+				</LinkWrapper>
 			</MasonaryTile>
 
 			<ExtraTallMasonaryTile onMouseEnter={() => setCurrentPhoto("discord")}>
-				<TitleText>Discord</TitleText>
-				<BackgroundImage
-					src={currentPhoto === "discord" ? discord_img : black_img}
-					show={currentPhoto === "discord"}
-				/>
+				<LinkWrapper href="https://discordapp.com/users/dillanp02">
+					<TitleText>Discord</TitleText>
+					<BackgroundImage
+						src={currentPhoto === "discord" ? discord_img : discord_blue}
+						show={currentPhoto === "discord"}
+					/>
+				</LinkWrapper>
 			</ExtraTallMasonaryTile>
 
-			<MasonaryTile onMouseEnter={() => setCurrentPhoto("youtube")}>
-				<TitleText>YouTube</TitleText>
-				<BackgroundImage
-					src={currentPhoto === "youtube" ? youtube_img : white_img}
-					show={currentPhoto === "youtube"}
-				/>
-			</MasonaryTile>
-
-			<WideMasonaryTile onMouseEnter={() => setCurrentPhoto("")}>
-				<TitleText>Letterboxd</TitleText>
+			<WideMasonaryTile onMouseEnter={() => setCurrentPhoto("youtube")}>
+				<LinkWrapper href="https://www.youtube.com/channel/UCGfFQhTL2rniAsImnQNt09A">
+					<TitleText>YouTube</TitleText>
+					<BackgroundImage
+						src={currentPhoto === "youtube" ? youtube_img : white_img}
+						show={currentPhoto === "youtube"}
+					/>
+				</LinkWrapper>
 			</WideMasonaryTile>
 
+			<MasonaryTile onMouseEnter={() => setCurrentPhoto("letterboxd")}>
+				<LinkWrapper href="https://letterboxd.com/dillanp02/">
+					<TitleText>Letterboxd</TitleText>
+					<BackgroundImage
+						src={letterboxd_img}
+						show={currentPhoto === "letterboxd"}
+					/>
+				</LinkWrapper>
+			</MasonaryTile>
+
 			<TallMasonaryTile onMouseEnter={() => setCurrentPhoto("bereal")}>
-				<TitleText>Bereal</TitleText>
-				<BackgroundImage
-					src={currentPhoto === "bereal" ? bereal_img : black_img}
-					show={currentPhoto === "bereal"}
-				/>
+				<LinkWrapper href="https://bere.al/dillanp02">
+					<TitleText>Bereal</TitleText>
+					<BackgroundImage
+						src={currentPhoto === "bereal" ? bereal_img : black_img}
+						show={currentPhoto === "bereal"}
+					/>
+				</LinkWrapper>
 			</TallMasonaryTile>
 
 			<WideTallMasonaryTile onMouseEnter={() => setCurrentPhoto("insta")}>
-				<TitleText>Instagram</TitleText>
-				<BackgroundImage
-					src={currentPhoto === "insta" ? instagram_img : white_img}
-					show={currentPhoto === "insta"}
-				/>
+				<LinkWrapper href="https://www.instagram.com/dillanp02/">
+					<TitleText>Instagram</TitleText>
+					<BackgroundImage
+						src={currentPhoto === "insta" ? instagram_img : white_img}
+						show={currentPhoto === "insta"}
+					/>
+				</LinkWrapper>
 			</WideTallMasonaryTile>
 
-			<MasonaryTile>
-				<TitleText>Reddit</TitleText>
+			<MasonaryTile onMouseEnter={() => setCurrentPhoto("reddit")}>
+				<LinkWrapper href="https://www.reddit.com/user/dillanp02">
+					<TitleText>Reddit</TitleText>
+					<BackgroundImage
+						src={currentPhoto === "reddit" ? reddit_img : white_img}
+						show={currentPhoto === "reddit"}
+					/>
+				</LinkWrapper>
 			</MasonaryTile>
 
-			<WideTallMasonaryTile>
-				<TitleText>LinkedIn</TitleText>
-			</WideTallMasonaryTile>
-
-			<ExtraTallMasonaryTile>
-				<TitleText>Twitch</TitleText>
+			<ExtraTallMasonaryTile onMouseEnter={() => setCurrentPhoto("twitch")}>
+				<LinkWrapper href="https://www.twitch.tv/dillanp02">
+					<TitleText>Twitch</TitleText>
+					<BackgroundImage
+						src={currentPhoto === "twitch" ? twitch_img : twitch_frame}
+						show={currentPhoto === "twitch"}
+					/>
+				</LinkWrapper>
 			</ExtraTallMasonaryTile>
+
+			<WideTallMasonaryTile onMouseEnter={() => setCurrentPhoto("linkedin")}>
+				<LinkWrapper href="https://www.linkedin.com/in/dillanp02/">
+					<TitleText>LinkedIn</TitleText>
+					<BackgroundImage
+						src={currentPhoto === "linkedin" ? linkedin_img : white_img}
+						show={currentPhoto === "linkedin"}
+					/>
+				</LinkWrapper>
+			</WideTallMasonaryTile>
 		</MasonaryContainer>
 	);
 }
